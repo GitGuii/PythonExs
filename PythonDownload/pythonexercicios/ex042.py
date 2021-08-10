e681a6ef081048afb678a6dc633725b11b2a3719
Guilherme Aparecido <guiap97@hotmail.com>
2021-08-10 14:20:51 -0300

    Update ex042.py

--- PythonDownload/pythonexercicios/ex042.py
+++ PythonDownload/pythonexercicios/ex042.py
@@ -1,13 +1,13 @@
-n1 = float(input('Digite o primeiro numero'))
-n2 = float(input('Digite o segundo numero'))
-n3 = float(input('Digite o terceiro numero'))
+n1 = float(input('Digite o primeiro numero: '))
+n2 = float(input('Digite o segundo numero: '))
+n3 = float(input('Digite o terceiro numero: '))
 if n1 < (n2 + n3) and n2 < (n1 + n3) and n3 < (n2 + n1):
-    print('Podem formar um triangulo')
+    print('Podem formar um triangulo ')
     if n1==n2 and n2 ==n3:
-        print('Triângulo de tipo Equilatero')
-    elif n1 == n2 and n2 !=n3:
-        print('Triângulo de tipo Isósceles')
+        print('Triângulo de tipo EQUILÁTERO!')
+    elif n1 == n2 and n2 !=n3 or n1 == n3 and n1 != n2 or n3 == n2 and n3 !=n1:
+        print('Triângulo de tipo ISÓSCELES!')
     elif n1 !=n2 and n2!=n3 and n1!=n3:
-        print('Triãngulo de tipo escaleno')
+        print('Triãngulo de tipo ESCALENO!')
 else:
-    print('Nao formam um triangulo')
+    print('Nao formam um triangulo!')
