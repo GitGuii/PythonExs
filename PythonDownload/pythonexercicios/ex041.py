3e979187113275782c6b2b5dacdcdd5ba1057676
Guilherme Aparecido <guiap97@hotmail.com>
2021-08-10 14:03:41 -0300

    Update ex041.py

--- PythonDownload/pythonexercicios/ex041.py
+++ PythonDownload/pythonexercicios/ex041.py
@@ -1,11 +1,14 @@
-idade = int(input('Digite a idade do atleta: '))
+from datetime import date, datetime
+atual = datetime.today().year
+nascimento = int(input('Digite o ano de nascimento do atleta: '))
+idade = atual - nascimento
 if idade <= 9:
     print('Atleta mirim.')
-elif idade > 9 or <= 14:
+elif idade > 9 and idade<= 14:
     print('Atleta infantil.')
-elif idade >14 or <=19:
+elif idade >14 and idade<=19:
     print('Atleta junior.')
-elif idade >19 or <=20:
+elif idade >19 and idade<=20:
     print('Atleta senior.')
 else:
     print('Atleta master.')
