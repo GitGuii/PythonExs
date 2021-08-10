78d2f2fe3665b07c588630fac49d158a03d771c3
Guilherme Aparecido <guiap97@hotmail.com>
2021-08-10 12:36:39 -0300

    Update ex039.py

--- PythonDownload/pythonexercicios/ex039.py
+++ PythonDownload/pythonexercicios/ex039.py
@@ -1,12 +1,19 @@
-Idade = int(input('Digite a idade do jovem: '))
-
+from datetime import date 
+atual = date.today().year
+nasc = int(input('Digite o ano de nascimento do jovem: '))
+Idade = atual - nasc
+print("Quem nasceu em {} tem {} anos em {}".format(nasc,Idade,atual))
 if Idade < 18:
     print('Ainda nao esta na hora de se alistar...')
     prazo = 18 - Idade
     print('Faltam {} anos para o alistamento'.format(prazo))
+    ano = prazo + atual
+    print("Seu alistamento sera em {}".format(ano))
 elif Idade ==18:
-    print('Esta na hora de se alistar')
+    print('Esta na hora de se alistar!!')
 elif Idade >18:
     print('Ja passou da idade de alistamento')
     prazo = Idade - 18
-    print('Já se passaram {} anos do prazo de alistamento'.format(prazo))
+    print('Já se passaram {} ano(s) do prazo de alistamento'.format(prazo))
+    ano = prazo - atual
+    print("O ano para alistamento foi em {}".format(ano))
