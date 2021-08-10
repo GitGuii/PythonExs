1315046c5f5968f44ebefb897878c9fcce11f8d1
Guilherme Aparecido <guiap97@hotmail.com>
2021-08-10 15:55:42 -0300

    Update ex045.py

--- PythonDownload/pythonexercicios/ex045.py
+++ PythonDownload/pythonexercicios/ex045.py
@@ -1,4 +1,48 @@
-import random 
+import sys
+from random import randint
+from time import sleep
+
 itens = ('pedra','papel','tesoura')
 computador = randint(0,2)
-print("O computador escolheu {}".format(computador))
+print('''Suas opçês
+[0] pedra
+[1] papel
+[2] tesoura''')
+jogador =int(input("Qual a sua escolha? "))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+sleep(1)
+print('-='*15)
+print("O computador jogou {}".format(itens[computador]))
+print("O jogador jogou {}".format(itens[jogador]))
+print('-='*15)
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCEU')
+    elif jogador == 2:
+        print('COMPUTADOR VENCEU')
+    else:
+        print("Opção invalida")
+elif computador == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCEU')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCEU')
+    else:
+        print("Opção invalida")
+elif computador == 2:
+    if jogador == 0:
+        print('JOGADOR VENCEU')
+    elif jogador == 1:
+        print('COMPUTADOR VENCEU')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print("Opção invalida")
